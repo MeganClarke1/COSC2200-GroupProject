8c8e0fd786d4665f6f54f8de82ee23731d6440e1
Brody Dentinger <brodydentinger@gmail.com>
2024-04-01 13:20:21 -0400

    - Updates to GameFunc., Round, Team, Trick.... StartGameState started for ViewModel fetch after start button

---
+++ Group2_COSC2200_Project/model/StartGameState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2_COSC2200_Project.model
+{
+    internal class StartGameState
+    {
+    }
+}
--- Group2_COSC2200_Project/model/Trick.cs
+++ Group2_COSC2200_Project/model/Trick.cs
@@ -11,6 +11,10 @@ namespace Group2_COSC2200_Project.model
     /// </summary>
     class Trick
     {
+        /// <summary>
+        /// A list of cards that have been played during the round.
+        /// </summary>
+        public List<Card> PlayedCards { get; private set; }
 
         ///<todo>Entire class. Ties closely with round, and game functionality. 
         ///Refer to all other made classes and flow Doc in teams.</todo>
