bb3af68c3426878c33bcc49ca6dd9a0d00ede9fe
Colin Eade <colin.eade@dcmail.ca>
2024-04-01 08:32:47 -0400

    Started GameView, and ViewModel dependancies

--- Group2_COSC2200_Project/App.xaml.cs
+++ Group2_COSC2200_Project/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Group2_COSC2200_Project.viewmodel;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,15 @@ namespace Group2_COSC2200_Project
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 
 }
---
+++ Group2_COSC2200_Project/viewmodel/CardViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Group2_COSC2200_Project.model;
+
+namespace Group2_COSC2200_Project.viewmodel
+{
+    public class CardViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// The Card model this ViewModel represents.
+        /// </summary>
+        public Card Card { get; private set; }
+
+        /// <summary>
+        /// Path to the image representing the Card.
+        /// </summary>
+        public string ImagePath
+        {
+            get
+            {
+                return $"../assets/images/classic/{Card.Rank.ToString().ToLower()}_of_{Card.Suit.ToString().ToLower()}.png";
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the CardViewModel class with a specified Card.
+        /// </summary>
+        /// <param name="card">The Card model to represent.</param>
+        public CardViewModel(Card card)
+        {
+            Card = card;
+        }
+    }
+}
