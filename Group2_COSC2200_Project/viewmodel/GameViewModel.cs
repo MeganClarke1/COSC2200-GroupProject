3909521899e68eb1f7741f669d59eae93d0dfd46
Colin Eade <colin.eade@dcmail.ca>
2024-04-02 21:41:01 -0400

    Have a look at Game and the viewmodel

--- Group2_COSC2200_Project/viewmodel/GameViewModel.cs
+++ Group2_COSC2200_Project/viewmodel/GameViewModel.cs
@@ -1,13 +1,77 @@
 ﻿using Group2_COSC2200_Project.model;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Windows.Input;
 
 namespace Group2_COSC2200_Project.viewmodel
 {
     public class GameViewModel : ViewModelBase
     {
+        private Game _game = new Game();
 
+        private HandViewModel _player1Hand;
+        private HandViewModel _player2Hand;
+        private HandViewModel _player3Hand;
+        private HandViewModel _player4Hand;
+
+        public HandViewModel Player1Hand
+        {
+            get => _player1Hand;
+            set
+            {
+                if (_player1Hand != value)
+                {
+                    _player1Hand = value;
+                    OnPropertyChanged(nameof(Player1Hand));
+                }
+            }
+        }
+
+        public HandViewModel Player2Hand
+        {
+            get => _player2Hand;
+            set
+            {
+                if (_player2Hand != value)
+                {
+                    _player2Hand = value;
+                    OnPropertyChanged(nameof(Player2Hand));
+                }
+            }
+        }
+
+        public HandViewModel Player3Hand
+        {
+            get => _player3Hand;
+            set
+            {
+                if (_player3Hand != value)
+                {
+                    _player3Hand = value;
+                    OnPropertyChanged(nameof(Player3Hand));
+                }
+            }
+        }
+
+        public HandViewModel Player4Hand
+        {
+            get => _player4Hand;
+            set
+            {
+                if (_player4Hand != value)
+                {
+                    _player4Hand = value;
+                    OnPropertyChanged(nameof(Player4Hand));
+                }
+            }
+        }
+
+        public GameViewModel()
+        {
+            _game.Initialize();
+            _player1Hand = new HandViewModel(_game.Players[0].PlayerHand);
+            _player2Hand = new HandViewModel(_game.Players[1].PlayerHand);
+            _player3Hand = new HandViewModel(_game.Players[2].PlayerHand);
+            _player4Hand = new HandViewModel(_game.Players[3].PlayerHand);
+        }
     }
 
 }
