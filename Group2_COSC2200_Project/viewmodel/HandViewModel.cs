e53fbca39b3749af4710326d9d02b13bc5b2aeca
Colin Eade <colin.eade@dcmail.ca>
2024-04-02 21:41:01 -0400

    Have a look at Game and the viewmodel

---
+++ Group2_COSC2200_Project/viewmodel/HandViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Group2_COSC2200_Project.model;
+using System.Collections.ObjectModel;
+
+namespace Group2_COSC2200_Project.viewmodel
+{
+    public class HandViewModel : ViewModelBase
+    {
+        public ObservableCollection<CardViewModel> Cards { get; }
+
+        public HandViewModel(Hand hand)
+        {
+            Cards = new ObservableCollection<CardViewModel>();
+
+            foreach (Card card in hand.Cards)
+            {
+                CardViewModel cardViewModel = new CardViewModel(card);
+                Cards.Add(cardViewModel);
+            }
+        }
+    }
+}
