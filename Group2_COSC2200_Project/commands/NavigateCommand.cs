cdc81dcc39ff80b90938ca35a27891f3576f4a20
Brody Dentinger <brodydentinger@gmail.com>
2024-04-08 10:58:31 -0400

    - Basic Navigation Routing complete from menu to game

---
+++ Group2_COSC2200_Project/commands/NavigateCommand.cs
@@ -0,0 +1,26 @@
+﻿using Group2_COSC2200_Project.stores;
+using Group2_COSC2200_Project.viewmodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2_COSC2200_Project.commands
+{
+    public class NavigateCommand : CommandBase
+    {
+
+        private readonly NavigationStore _navigationStore;
+
+        public NavigateCommand(NavigationStore navigationStore)
+        {
+            _navigationStore = navigationStore;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _navigationStore.CurrentViewModel = new GameViewModel(); // I think we can pass data here ? into the constructor of the GameViewModel?
+        }
+    }
+}
